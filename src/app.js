0aa1d05e9cf624143f7cfa7b35ebda531ea01164
raptor-2001 <pratham007yadav@gmail.com>
2023-01-19 10:22:44 +0530

    post-adding functionality created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 import {http} from './http'
 import {ui} from './ui'
 
+
+// DOM for post
 document.addEventListener('DOMContentLoaded',getPosts);
 
+// Get Post
 function getPosts(){
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
   .catch(err => console.log(err))
+}
+
+// DOM for submit post
+const submitButton = document.querySelector('.post-submit');
+submitButton.addEventListener('click',submitPost);
+
+
+
+
+// Submit Post
+function submitPost(){
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title: title,
+    body: body
+  }
+
+  http.post('http://localhost:3000/posts',data)
+  .then(data => 
+      getPosts(),
+      ui.showAlerts("Post added", "alert alert-success"),
+      ui.clearFields()
+    )
+  .catch(err => console.log(err));
 }
 
--- src/ui.js
+++ src/ui.js
@@ -10,27 +10,70 @@ class UI{
   }
 
   showPosts(posts){
-    let output;
+    let output="";
 
     posts.forEach(post => {
       output+=`
-        <div class="card mb-3">
-          <div class="card-body">
-            <h4 class="card-title">${post.title}</h4>
-            <p class="card-text">${post.body}</p>
-            <a href="#" class="edit card-link" data-id="${post.id}>
-              <i class="fa fa-pencil"></i>
-            </a>
-            <a href="#" class="delete card-link" data-id="${post.id}>
-              <i class="fa fa-remove"></i>
-            </a>
-          </div>
-        </div>
+      <div class="card mb-3">
+      <div class="card-body">
+        <h4 class="card-title">${post.title}</h4>
+        <p class="card-text">${post.body}</p>
+        <a href="#" class="edit card-link" data-id="${post.id}">
+        <i class="fa fa-pencil"></i>
+        </a>
+
+        <a href="#" class="delete card-link" data-id="${post.id}">
+        <i class="fa fa-trash"></i>
+      </a>
+      </div>
+    </div>
       `
 
     });
     this.post.innerHTML = output;
     
+  }
+
+  showAlerts(message, alertClass){
+    this.clearAlert();
+
+    // create div
+    const div = document.createElement('div');
+    div.className = alertClass;
+    div.appendChild(document.createTextNode(message));
+
+    // parent element
+    const postContainer = document.querySelector('.postsContainer');
+
+    // neighbour
+    const posts = document.querySelector('#posts');
+
+    // insert div
+    postContainer.insertBefore(div,posts);
+
+    setTimeout(() => {
+      this.clearAlert()
+    },3000);
+
+
+  }
+
+  clearAlert(){
+
+    
+    const alertDiv = document.querySelector('.alert');
+
+    if(alertDiv){
+      alertDiv.remove();
+    }
+
+  }
+
+  clearFields(){
+
+     this.bodyInput.value = "";
+     this.titleInput.value = "";
+
   }
 }
 
